d3b49540ea65d6a554d05591dd03571d49226ccd
alextkachuk3 <tkachuk.alexey@unicyb.kiev.ua>
2023-12-20 07:03:22 +0200

    Display selected cover on music upload page

--- frontend/frontend/src/app/upload/upload.component.ts
+++ frontend/frontend/src/app/upload/upload.component.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 import { Router, RouterLink } from "@angular/router";
 import { FormsModule } from "@angular/forms";
 import { HttpClient } from "@angular/common/http";
+import { NgIf } from "@angular/common";
 
 import { AuthorizationService } from "../authorization/authorization.service";
 import { UploadService } from "./upload.service";
@@ -9,17 +10,32 @@ import { UploadService } from "./upload.service";
 @Component({
   selector: "upload",
   standalone: true,
-  imports: [RouterLink, FormsModule],
+  imports: [RouterLink, FormsModule, NgIf],
   templateUrl: './upload.component.html',
   providers: [UploadService, AuthorizationService, FormsModule]
 })
 export class UploadComponent {
   musicName: string = '';
   selectedFile: File | null = null;
+  coverFile: File | null = null;
+  imageUrl: string | ArrayBuffer | null = null;
   constructor(private uploadService: UploadService, private authService: AuthorizationService, private http: HttpClient, private router: Router) { }
 
   onFileSelected(event: any): void {
     this.selectedFile = event.target.files[0] as File;
+  }
+
+  onCoverSelected(event: any): void {
+    this.coverFile = event.target.files[0];
+
+    if (this.coverFile) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        this.imageUrl = e.target.result;
+      };
+
+      reader.readAsDataURL(this.coverFile);
+    }
   }
 
   onSubmit(): void {
